51cd30ecab1f10ee05126de051757b84279f0981
JorgeSchelotto <jasfotografo@hotmail.com>
2020-10-20 02:14:06 -0300

    Cambios index

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,12 +6,13 @@
     
     // Get the offset position of the navbar
     var sticky = navbar.offsetTop;
-    
+    navbar.classList.add("sticky")
     // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
     function myFunction() {
         if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
+            navbar.classList.add("sticky")
         } else {
-        navbar.classList.remove("sticky");
+            navbar.classList.remove("sticky");
         }
     }
+
